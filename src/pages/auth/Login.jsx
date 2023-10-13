530110186d089039cdb1cfe3a73b25d1a38eb4b5
vuhung1402 <badao867@gmail.com>
2023-10-13 14:55:09 +0700

    fetch api login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,10 +1,44 @@
+import { useRef, useState } from "react"
 import { useUserPackageHook } from "../../redux/hooks/userHook"
+import { json } from "react-router-dom"
 
 const Login = () => {
     const user = useUserPackageHook()
     console.log(user)
+    const [response, setResponse] = useState()
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const handleLogin = () =>{
+        console.log("email: ", emailRef.current.value)
+        console.log("pass: ", passwordRef.current.value)
+        const body = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value
+        }
+
+        fetch("http://localhost:3001/api/auth/login", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            if(!response.ok){
+                throw new Error("Netword response not ok")
+            }
+            return response.json()
+        }).then((json) => {
+            setResponse(json)
+            console.log("Response: ", json)
+        }).catch((error) => {
+            console.error("Error: ", error)
+        })
+    }
+
     return(
-        <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a
                     href="/"
@@ -20,41 +54,46 @@ const Login = () => {
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Sign in to your account
+                        Sign in to your account
                     </h1>
-                    <form className="space-y-4 md:space-y-6" action="#">
+                    <div className="space-y-4 md:space-y-6">
                     <div>
                         <label
-                        htmlFor="email"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            htmlFor="email"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                         >
-                        Your email
+                            Your email
                         </label>
                         <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="name@company.com"
-                        required=""
+                            ref={emailRef}
+                            type="email"
+                            name="email"
+                            id="email"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="name@company.com"
+                            required=""
                         />
                     </div>
                     <div>
                         <label
-                        htmlFor="password"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            htmlFor="password"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                         >
-                        Password
+                            Password
                         </label>
                         <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        placeholder="••••••••"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required=""
+                            ref={passwordRef}
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="••••••••"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required=""
                         />
                     </div>
+                    <button onClick={handleLogin} type="button" className=" w-full text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Login
+                    </button>
                     <div className="flex items-center justify-between">
                         <div className="flex items-start">
                         <div className="flex items-center h-5">
@@ -68,18 +107,18 @@ const Login = () => {
                         </div>
                         <div className="ml-3 text-sm">
                             <label
-                            htmlFor="remember"
-                            className="text-gray-500 dark:text-gray-300"
+                                htmlFor="remember"
+                                className="text-gray-500 dark:text-gray-300"
                             >
-                            Remember me
+                                Remember me
                             </label>
                         </div>
                         </div>
                         <a
-                        href="#"
-                        className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                            href="#"
+                            className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
                         >
-                        Forgot password?
+                            Forgot password?
                         </a>
                     </div>
                     <button
@@ -97,11 +136,11 @@ const Login = () => {
                             Sign up
                         </a>
                     </p>
-                    </form>
+                    </div>
                 </div>
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 
