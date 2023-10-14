31892616b22d291ef5a2a7d116ba3aa00fb2e65c
vuhung1402 <badao867@gmail.com>
2023-10-14 21:56:12 +0700

    forgotPass page

--- src/pages/auth/ForgotPass.jsx
+++ src/pages/auth/ForgotPass.jsx
@@ -1,11 +1,36 @@
+import { useRef } from "react"
+import { useNavigate } from "react-router-dom"
 
 const ForgotPass = () => {
+    const emailRef = useRef()
+
+    const navigate = useNavigate();
+
+    const handleResetPass = () => {
+        console.log("email: ", emailRef.current.value)
+
+        // fetch("http://localhost:3001/api/auth/forgot-password", {
+        //     method: "GET",
+        // }).then((response) => {
+        //     if(!response.ok){
+        //         throw new Error("Netword response not ok")
+        //     }
+        //     return response.json()
+        // }).then((json) => {
+        //     setResponse(json)
+        //     console.log("Response: ", json)
+        // }).catch((error) => {
+        //     console.error("Error: ", error)
+        // })
+        navigate(`/resetPass`)
+    }
+
     return(
-        <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a
-                href="/"
-                className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+                    href="/"
+                    className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
                 >
                 <img
                     className="w-8 h-8 mr-2"
@@ -16,93 +41,32 @@ const ForgotPass = () => {
                 </a>
                 <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
                 <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Change Password
+                    Your email
                 </h2>
-                <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
+                <div className="mt-4 space-y-4 lg:mt-5 md:space-y-5">
                     <div>
-                    <label
-                        htmlFor="email"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Your email
-                    </label>
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="name@company.com"
-                        required=""
-                    />
-                    </div>
-                    <div>
-                    <label
-                        htmlFor="password"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        New Password
-                    </label>
-                    <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        placeholder="••••••••"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required=""
-                    />
-                    </div>
-                    <div>
-                    <label
-                        htmlFor="confirm-password"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Confirm password
-                    </label>
-                    <input
-                        type="confirm-password"
-                        name="confirm-password"
-                        id="confirm-password"
-                        placeholder="••••••••"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required=""
-                    />
-                    </div>
-                    <div className="flex items-start">
-                    <div className="flex items-center h-5">
                         <input
-                        id="newsletter"
-                        aria-describedby="newsletter"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                        required=""
+                            ref={emailRef}
+                            type="email"
+                            name="email"
+                            id="email"
+                            className="bg-gray-50 border outline-none border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="name@company.com"
+                            required=""
                         />
                     </div>
-                    <div className="ml-3 text-sm">
-                        <label
-                        htmlFor="newsletter"
-                        className="font-light text-gray-500 dark:text-gray-300"
-                        >
-                        I accept the{" "}
-                        <a
-                            className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                            href="#"
-                        >
-                            Terms and Conditions
-                        </a>
-                        </label>
-                    </div>
-                    </div>
+                        
                     <button
-                    type="submit"
-                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                        onClick={handleResetPass}
+                        type="button"
+                        className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                     >
-                    Reset passwod
+                        Reset Password
                     </button>
-                </form>
+                </div>
                 </div>
             </div>
-        </section>
-
+        </div>
     )
 }
 
