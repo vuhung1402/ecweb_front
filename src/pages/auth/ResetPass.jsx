ab407b142979fa6a12b0fdd4a2a912d5c7da0b8b
vuhung1402 <badao867@gmail.com>
2023-10-14 21:56:42 +0700

    resetPass page

---
+++ src/pages/auth/ResetPass.jsx
@@ -0,0 +1,126 @@
+import { useRef, useState } from "react"
+
+const ResetPass = () => {
+    const [response, setResponse] = useState()
+
+    const emailRef = useRef()
+    const newPassRef = useRef()
+    const confirmPassRef = useRef()
+
+    const handleChangePass = () => {
+        console.log("email: ", emailRef.current.value)
+        console.log("new pass: ", newPassRef.current.value)
+        console.log("confirm pass: ", confirmPassRef.current.value)
+
+        const body = {
+            password: newPassRef.current.value,
+            confirmPassword: confirmPassRef.current.value
+        }
+
+        fetch("http://localhost:3001/api/auth/forgot-password", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            if(!response.ok){
+                throw new Error("Netword response not ok")
+            }
+            return response.json()
+        }).then((json) => {
+            setResponse(json)
+            console.log("Response: ", json)
+        }).catch((error) => {
+            console.error("Error: ", error)
+        })
+    }
+
+
+    return(
+        <div className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <a
+                href="/"
+                className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+                >
+                <img
+                    className="w-8 h-8 mr-2"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                    alt="logo"
+                />
+                Flowbite
+                </a>
+                <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+                <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Change Password
+                </h2>
+                <div className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
+                    <div>
+                    <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        Your email
+                    </label>
+                    <input
+                        ref={emailRef}
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="name@company.com"
+                        required=""
+                    />
+                    </div>
+                    <div>
+                    <label
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        New Password
+                    </label>
+                    <input
+                        ref={newPassRef}
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required=""
+                    />
+                    </div>
+                    <div>
+                    <label
+                        htmlFor="confirm-password"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        Confirm password
+                    </label>
+                    <input
+                        ref={confirmPassRef}
+                        type="password"
+                        name="confirm-password"
+                        id="confirm-password"
+                        placeholder="••••••••"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required=""
+                    />
+                    </div>
+                    <button
+                        onClick={handleChangePass}
+                        type="button"
+                        className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    >
+                        Change Password
+                    </button>
+
+                </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default ResetPass
