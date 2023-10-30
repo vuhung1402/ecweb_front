442cf582e564be234526caaa7f42d72c510e5446
vuhung1402 <badao867@gmail.com>
2023-10-30 12:16:23 +0700

    address

---
+++ src/component/Address.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react"
+import AddressInfor from "./AddressInfor"
+import { useUserPackageHook } from "../redux/hooks/userHook"
+import UpdateAddress from "./UpdateAddress"
+import DeletePopUp from "./DeletePopUp"
+
+const Address = () => {
+
+    const data = useUserPackageHook()
+
+    const [addressPopUp, setAddressPopUp] = useState(false)
+    const [updateAddress, setUpdateAddress] = useState(false)
+    const [deleteAddress, setDeleteAddress] = useState(false)
+    const [addresses, setAddresses] = useState()
+
+    useEffect(() => {
+        fetch("https://tiny-jade-elk-wear.cyclic.cloud/api/users/me/addresses", {
+            method: "GET",
+            headers: {
+                'Authorization': `Bearer ${data?.accessToken}`,
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            if(!response.ok){
+                throw new Error("Netword response not ok")
+            }
+            return response.json()
+        }).then((json) => {
+            if(json?.success){
+                setAddresses(json?.data?.addresses)
+            }
+        }).catch((error) => {
+            console.error("Error: ", error)
+        })
+    },[updateAddress,deleteAddress,addresses])
+
+    return(
+        <div className=" p-2 h-screen overflow-y-scroll scrollbar-hide">
+            {addressPopUp && <AddressInfor setAddressPopUp= {setAddressPopUp} />}
+            <button onClick={() => setAddressPopUp(true)} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                Add address
+            </button>
+            {
+                addresses?.map((address) => {
+                    return(
+                        <div className=" border-t-2">
+                            { updateAddress && <UpdateAddress address = {address} setUpdateAddress = {setUpdateAddress} /> }
+                            { deleteAddress && <DeletePopUp addressId = {address?._id} setDeleteAddress = {setDeleteAddress} />}
+                            <div className="flex items-center justify-between p-8 border-b-2">
+                                <div>
+                                    <div className="p-1">{address?.name}</div>
+                                    <div className="p-1">{address?.phone}</div>
+                                    <div className="p-1">{address?.ward}, {address?.district}, {address?.city}</div>
+                                    <div className="p-1">{address?.detail}</div>
+                                </div>
+                                <div>
+                                    <button onClick={() => setUpdateAddress(true)} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                        Update
+                                    </button>
+                                    <button onClick={() => setDeleteAddress(true)} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                        Delete
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            
+        </div>
+    )
+}
+
+export default Address
