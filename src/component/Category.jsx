9867ad3e720baa6e6934e70a3b71ad23973c6be0
vuhung1402 <badao867@gmail.com>
2023-11-05 18:48:00 +0700

    fetch data from api

--- src/component/Category.jsx
+++ src/component/Category.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react"
+import Loading from "./Loading"
+import { endpoint } from "../api"
 
 const Category = () => {
+
+    const [categories, setCategories] = useState()
+
+    useEffect(() => {
+        fetch(`${endpoint}/categories`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            if(!response.ok){
+                throw new Error("Netword response not ok")
+            }
+            return response.json()
+        }).then((json) => {
+            if(json?.success){
+                setCategories(json?.data?.categories)
+                console.log("json: ", json)
+            }
+        }).catch((error) => {
+            console.error("Error: ", error)
+        })
+    },[])
+
+    console.log(categories)
+
     const callouts = [
         {
           name: 'Desk and Office',
@@ -27,26 +56,31 @@ const Category = () => {
         <div className="bg-gray-100">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-10">
-                <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+                <h2 className="text-2xl font-bold text-gray-900">CATEGORIES</h2>
+                { !categories &&
+                    (
+                        <Loading/>
+                    )
+                }
 
                 <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-                    {callouts.map((callout) => (
-                    <div key={callout.name} className="group relative">
-                        <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                        <img
-                            src={callout.imageSrc}
-                            alt={callout.imageAlt}
-                            className="h-full w-full object-cover object-center"
-                        />
+                    {categories?.map((category) => (
+                        <div key={category.slug} className="group relative">
+                            <div className="relative mt-3 h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                                <img
+                                    src='https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg'
+                                    alt="img"
+                                    className="h-full w-full object-cover object-center"
+                                />
+                            </div>
+                            {/* <h3 className="mt-6 text-sm text-gray-500">
+                                <a href="#">
+                                    <span className="absolute inset-0" />
+                                    {category.name}
+                                </a>
+                            </h3> */}
+                            <p className="text-base font-semibold text-gray-900">{category.name}</p>
                         </div>
-                        <h3 className="mt-6 text-sm text-gray-500">
-                        <a href={callout.href}>
-                            <span className="absolute inset-0" />
-                            {callout.name}
-                        </a>
-                        </h3>
-                        <p className="text-base font-semibold text-gray-900">{callout.description}</p>
-                    </div>
                     ))}
                 </div>
                 </div>
