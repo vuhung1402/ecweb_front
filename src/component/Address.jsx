e0b00f84797ce30141395547b0f75de235852236
vuhung1402 <badao867@gmail.com>
2023-11-05 18:47:48 +0700

    use variable api

--- src/component/Address.jsx
+++ src/component/Address.jsx
@@ -3,6 +3,10 @@ import AddressInfor from "./AddressInfor"
 import { useUserPackageHook } from "../redux/hooks/userHook"
 import UpdateAddress from "./UpdateAddress"
 import DeletePopUp from "./DeletePopUp"
+import SuccessAlert from "./SuccesAlert"
+import UnsuccessAlert from "./UnsuccessAlert"
+import Loading from "./Loading"
+import { endpoint } from "../api"
 
 const Address = () => {
 
@@ -11,10 +15,12 @@ const Address = () => {
     const [addressPopUp, setAddressPopUp] = useState(false)
     const [updateAddress, setUpdateAddress] = useState(false)
     const [deleteAddress, setDeleteAddress] = useState(false)
+    const [successAlert, setSuccessAlert] = useState(false)
+    const [unsuccessAlert, setUnsuccessAlert] = useState(false)
     const [addresses, setAddresses] = useState()
 
     useEffect(() => {
-        fetch("https://tiny-jade-elk-wear.cyclic.cloud/api/users/me/addresses", {
+        fetch(`${endpoint}/users/me/addresses`, {
             method: "GET",
             headers: {
                 'Authorization': `Bearer ${data?.accessToken}`,
@@ -37,14 +43,17 @@ const Address = () => {
     return(
         <div className=" p-2 h-screen overflow-y-scroll scrollbar-hide">
             {addressPopUp && <AddressInfor setAddressPopUp= {setAddressPopUp} />}
+            { successAlert && <SuccessAlert/>}
+            { unsuccessAlert && <UnsuccessAlert/>}
             <button onClick={() => setAddressPopUp(true)} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                 Add address
             </button>
+            { !addresses && <Loading/>}
             {
                 addresses?.map((address) => {
                     return(
                         <div className=" border-t-2">
-                            { updateAddress && <UpdateAddress address = {address} setUpdateAddress = {setUpdateAddress} /> }
+                            { updateAddress && <UpdateAddress address = {address} setSuccessAlert = {setSuccessAlert} setUnsuccessAlert = {setUnsuccessAlert} setUpdateAddress = {setUpdateAddress} /> }
                             { deleteAddress && <DeletePopUp addressId = {address?._id} setDeleteAddress = {setDeleteAddress} />}
                             <div className="flex items-center justify-between p-8 border-b-2">
                                 <div>
