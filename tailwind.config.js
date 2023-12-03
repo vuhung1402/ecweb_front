7815dc7a2de3fa46779841b3c19cbda47efe6754
vuhung1402 <badao867@gmail.com>
2023-12-03 21:42:32 +0700

    add lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('tailwind-scrollbar-hide')
+    require('tailwind-scrollbar-hide'),
+    require('flowbite/plugin')
   ],
 }
