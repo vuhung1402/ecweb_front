be86e684f970b8b7b5259a3ef4834eacd93383e5
vuhung1402 <badao867@gmail.com>
2023-12-05 10:24:11 +0700

    add param status

--- src/api.js
+++ src/api.js
@@ -1,1 +1,10 @@
 export const endpoint = "https://ecwebsite.onrender.com/api"
+
+export const status = {
+    "to-pay": "Chờ thanh toán",
+    "to-confirm": "Chờ xác nhận",
+    "to-ship": "Chờ lấy hàng",
+    "to-receive": "Đang giao",
+    "completed": "Đã nhận hàng",
+    "canceled": "Huỷ đơn"
+}
