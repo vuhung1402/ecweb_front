1008ecb65f41959304497c34b77bf0d20b22c048
vuhung1402 <badao867@gmail.com>
2023-12-26 11:21:27 +0700

    update code

--- src/pages/seller/ShopPage.jsx
+++ src/pages/seller/ShopPage.jsx
@@ -3,18 +3,20 @@ import HeaderSeller from "../../component/HeaderSeller"
 import OrderSeller from "./OrderSeller"
 import ProductListSeller from "./Products"
 import SildeBar from "./SildeBar"
+import InforShop from "./InforShop"
 
 const ShopPage = () => {
     const [sildeBar, setSildeBar] = useState(1)
 
     return(
-        <div>
+        <div className=" h-auto">
             <HeaderSeller isSeller = {true} />
-            <div className="flex">
-                <div className=" w-1/5">
+            <div className="flex h-auto">
+                <div className=" w-1/5 h-auto">
                     <SildeBar setSildeBar = {setSildeBar} />
                 </div>
                 <div className=" w-4/5">
+                    {sildeBar === 1 && <InforShop/>}
                     {sildeBar === 3 && <ProductListSeller/>}
                     {sildeBar === 4 && <OrderSeller/>}
                 </div>
