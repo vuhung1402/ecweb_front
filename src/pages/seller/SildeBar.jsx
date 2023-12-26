ced49dca5d52e11a4b719e50297e6b7134fb2786
vuhung1402 <badao867@gmail.com>
2023-12-26 11:21:44 +0700

    update code

--- src/pages/seller/SildeBar.jsx
+++ src/pages/seller/SildeBar.jsx
@@ -11,7 +11,7 @@ const SildeBar = ({setSildeBar}) => {
                 </svg>
             </button>
 
-            <aside id="default-sidebar" class="fixed left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+            <aside id="default-sidebar" class=" left-0 z-40 w-64 h-auto transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
                 <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
                     <ul class="space-y-2 font-medium">
                         <li>
@@ -21,15 +21,6 @@ const SildeBar = ({setSildeBar}) => {
                                     <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
                                 </svg>
                                 <span class="ms-3">Thông tin cửa hàng</span>
-                            </div>
-                        </li>
-                        <li>
-                            <div onClick={() => setSildeBar(2)} class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-                                </svg>
-                                <span class="flex-1 ms-3 whitespace-nowrap">Vận chuyển</span>
-
                             </div>
                         </li>
                         <li>
