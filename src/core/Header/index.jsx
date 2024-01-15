79bfdf40441e50fbc63b49b6b0893970480d6393
vuhung1402 <badao867@gmail.com>
2024-01-15 09:21:02 +0700

    update header

--- src/core/Header/index.jsx
+++ src/core/Header/index.jsx
@@ -1,307 +1,118 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { useUserPackageHook } from "../../redux/hooks/userHook"
 import { useNavigate } from "react-router-dom"
+import './style.scss'
+import SearchBox from "../../component/SearchBox/SearchBox"
+import CartPopUp from "../../component/CartPopUp/CartPopUp"
 
 const Header = () => {
-    const searchRef = useRef()
-
-    const data = useUserPackageHook()
-    const navigate = useNavigate()
-
-    const handleSearch = () => {
-      console.log("Search: ", searchRef.current.value)
-      navigate(
-        `/products`, 
-        {
-          state:{
-            search: searchRef.current.value
-          } 
-        }
-      )
-    }
-
-    const handleClick = () => {
-      navigate('/profile')
-    } 
-
-    const handleCart = () => {
-      navigate('/cart')
-    }
-
-    console.log("user dat: ", data);
+  const [searchBox, setSearchBox] = useState(false)
+  const [account, setAccount] = useState(false)
+  const [cartPopUp, setCartPopUp] = useState(false)
 
     return(
-        <header className="bg-white border-b-2">
-          {data?.user?.isSeller ? 
-            <a href="/shopPage" className=" p-1 text-blue-500 cursor-pointer underline">Kênh người bán</a> : 
-            <a href="/registerSeller" className=" p-1 text-blue-500 cursor-pointer underline">Đăng ký bán hàng</a>
-          }
-
-          {
-            data?.user?.isAdmin &&
-            <a href="/admin" className=" p-1 text-blue-500 cursor-pointer underline">Admin</a>
-          }
-
-          <nav
-            className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-            aria-label="Global"
-          >
-            <div className="flex lg:flex-1">
-              <a href="/" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
-              </a>
-            </div>
-            <div className="flex lg:hidden">
-              <button
-                type="button"
-                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              >
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="hidden lg:flex lg:gap-x-12">
-              {/* <div className="relative">
-                <a
-                  href={"/products"}
-                  className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Product
-                </a>
-              </div> */}
-              {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Features
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Marketplace
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Company
-              </a> */}
-            </div>
-            {/* search */}
-            <div className="ml-3 w-[500px]">
-              <label
-                htmlFor="default-search"
-                className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-              >
-                Search
-              </label>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg
-                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="search"
-                  ref={searchRef}
-                  id="default-search"
-                  className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white outline-none"
-                  placeholder="Search Mockups, Logos..."
-                  required=""
-                />
-                <button
-                  onClick={handleSearch}
-                  type="submit"
-                  className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Search
-                </button>
-              </div>
-            </div>
-            <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:items-center">
-              <div onClick={handleCart} className=" mr-10 cursor-pointer">
+        <header>
+          <div>LOGO</div>
+          <nav class="menu">
+            <ul>
+                <li><a href="/">TRANG CHỦ</a></li>
+                <li>
+                    <a href="#">SẢN PHẨM ▾</a>
+                    <ul>
+                        <li>
+                            <a href="#"> ÁO ▾</a>
+                            <ul className=" border">
+                                <li><a href="#">XEM TẤT CẢ ÁO</a></li>
+                                <li><a href="#">ÁO THUN</a></li>
+                                <li><a href="#">ÁO SƠ MI</a></li>
+                                <li><a href="#">ÁO POLO</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">CỬA HÀNG</a></li>
+                        <li>
+                            <a href="#">CHÍNH SÁCH ▾</a>
+                            <ul>
+                                <li><a href="#">CHÍNH SÁCH ĐỔI TRẢ</a></li>
+                                <li><a href="#">CHÍNH SÁCH BẢO MẬT</a></li>
+                                <li><a href="#">CHÍNH SÁCH BẢO MẬT</a></li>
+                                <li><a href="#">CHÍNH SÁCH BẢO MẬT</a></li>
+                                <li><a href="#">CHÍNH SÁCH BẢO MẬT</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#">CỬA HÀNG</a></li>
+                <li>
+                  <a href="#">CHÍNH SÁCH ▾</a>
+                  <ul>
+                    <li><a>CHÍNH SÁCH ĐỔI TRẢ</a></li>
+                    <li><a>CHÍNH SÁCH BẢO MẬT</a></li>
+                    <li><a>CHÍNH SÁCH GIAO HÀNG</a></li>
+                    <li><a>PHƯƠNG THỨC THANH TOÁN</a></li>
+                    <li><a>HƯỚNG DẪN MUA HÀNG</a></li>
+                  </ul>
+                </li>
+            </ul>
+          </nav>
+          <div className=" flex justify-between w-[132px]">
+            <div className=" relative cursor-pointer">
+              <div onClick={() => {
+                setSearchBox(!searchBox) 
+                setAccount(false)
+                setCartPopUp(false)
+              }}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                 </svg>
               </div>
               {
-                data?.user ?
+                searchBox && 
                 (
-                  <div onClick={handleClick} className="flex items-center justify-between cursor-pointer">
-                    <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                      <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                    </div>
-                     <div className=" ml-3">{data?.user?.email.slice(0, data?.user?.email.indexOf("@"))}</div>
+                  <div className=" absolute bg-white rounded-md shadow-slate-600 border top-10 right-1 w-[370px]">
+                    <SearchBox/>
                   </div>
-                )
-                :
-                (
-                  <a href={"/login"} className="text-sm font-semibold leading-6 text-gray-900 cursor-pointer">
-                    Login
-                  </a>
                 )
               }
-              {/* <a href={"/login"} className="text-sm font-semibold leading-6 text-gray-900 cursor-pointer">
-                Login
-              </a> */}
             </div>
-          </nav>
-          {/* Mobile menu, show/hide based on menu open state. */}
-          <div className="lg:hidden" role="dialog" aria-modal="true">
-            {/* Background backdrop, show/hide based on slide-over state. */}
-            <div className="fixed inset-0 z-10" />
-            <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-              <div className="flex items-center justify-between">
-                <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">Your Company</span>
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt=""
-                  />
-                </a>
-                <button
-                  type="button"
-                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                >
-                  <span className="sr-only">Close menu</span>
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
+            <div className=" cursor-pointer relative">
+              <div onClick={() => {
+                setAccount(!account)
+                setSearchBox(false)
+                setCartPopUp(false)
+              }}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
               </div>
-              <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="space-y-2 py-6">
-                    <div className="-mx-3">
-                      <button
-                        type="button"
-                        className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        aria-controls="disclosure-1"
-                        aria-expanded="false"
-                      >
-                        Product
-                        <svg
-                          className="h-5 w-5 flex-none"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </button>
-                      {/* 'Product' sub-menu, show/hide based on menu state. */}
-                      <div className="mt-2 space-y-2" id="disclosure-1">
-                        <a
-                          href="#"
-                          className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                          Analytics
-                        </a>
-                        <a
-                          href="#"
-                          className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                          Engagement
-                        </a>
-                        <a
-                          href="#"
-                          className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                          Security
-                        </a>
-                        <a
-                          href="#"
-                          className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                          Integrations
-                        </a>
-                        <a
-                          href="#"
-                          className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                          Automations
-                        </a>
-                        <a
-                          href="#"
-                          className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                          Watch demo
-                        </a>
-                        <a
-                          href="#"
-                          className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                          Contact sales
-                        </a>
-                      </div>
-                    </div>
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      Features
-                    </a>
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      Marketplace
-                    </a>
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      Company
-                    </a>
+              {
+                account && 
+                (
+                  <div className=" absolute right-1 top-10 w-[220px] bg-white rounded border p-2">
+                    <div className=" uppercase text-center font-semibold p-2 border-b-[1px]">Thông tin tài khoản</div>
+                    <div className=" hover:text-blue-300 cursor-pointer">Tài khoản của tôi</div>
+                    <div className="hover:text-blue-300 cursor-pointer">Đăng xuất</div>
                   </div>
-                  <div className="py-6">
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      Log in
-                    </a>
-                  </div>
-                </div>
+                )
+              }
+            </div>
+            <div className=" relative cursor-pointer" >
+              <div onClick={() => {
+                setAccount(false)
+                setSearchBox(false)
+                setCartPopUp(!cartPopUp)
+              }}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                </svg>
               </div>
+              {
+                cartPopUp &&
+                (
+                  <div className=" absolute top-10 w-[450px] right-0 bg-white rounded border p-2">
+                    <CartPopUp/>
+                  </div>
+                )
+              }
             </div>
           </div>
         </header>
