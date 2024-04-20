4a731ba307ed74b562cf5119eab27d6d47f4b7e5
vuhung1402 <badao867@gmail.com>
2024-04-20 22:41:29 +0700

    profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react"
 import { useUserPackageHook } from "../../redux/hooks/userHook"
 import Address from "../../component/Address"
 import OrderHistory from "../../component/OrderHistory"
+import { endpoint } from "../../api"
 
 
 const Profile = () => {
@@ -15,6 +16,40 @@ const Profile = () => {
 
     const user = useUserPackageHook()
 
+    const [infor, setInfor] = useState()
+
+    const myAlert = () => {
+        const response = window.confirm("Vui lòng đăng nhập lại!!!");
+        if(response){
+            dispatch(clear());
+            navigate("/login")
+        }
+    }
+
+    useEffect(() => {
+        fetch(`${endpoint}/users/get_info/${user?.data}/`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            if(!response.ok){
+                throw new Error("Netword response not ok")
+            }
+            return response.json()
+        }).then((json) => {
+            
+            if(json?.success){
+                setInfor(json)
+            }else{
+                // myAlert()
+                console.log("false")
+            }
+        }).catch((error) => {
+            console.error("Error: ", error)
+        })
+    }, [])
+
     const [content, setContent] = useState(1)
 
     const handleLogOut = () => {
@@ -23,63 +58,76 @@ const Profile = () => {
     }
     
     return( 
-        // <button onClick={handleLogOut} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-        //     Log out
-        // </button>
-        <div className="flex h-auto">
-            <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                <span class="sr-only">Open sidebar</span>
-                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-                </svg>
-            </button>
-
-            <aside id="default-sidebar" className="w-64 h-auto border-r-2 transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                    <ul class="space-y-2 font-medium">
-                        <li onClick={() => setContent(1)}>
-                            <div className="cursor-pointer flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                                <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-                            </svg>
-                            <span class="flex-1 ml-3 whitespace-nowrap">Information</span>
-                            </div>
-                        </li>
-                        <li onClick={() => setContent(2)}>
-                            <div className="cursor-pointer flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                            </svg>
-                            <span class="flex-1 ml-3 whitespace-nowrap">Address</span>
-                            </div>
-                        </li>
-                        <li onClick={() => setContent(3)}>
-                            <div className=" cursor-pointer flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                                <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
-                            </svg>
-                            <span class="flex-1 ml-3 whitespace-nowrap">Order</span>
-                            </div>
-                        </li>
-                        <li>
-                            <div onClick={handleLogOut} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group cursor-pointer">
-                                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-                                    <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-                                    <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-                                </svg>
-                                <span class="flex-1 ml-3 whitespace-nowrap">Log out</span>
-                            </div>
-                        </li>
-                    </ul>
+        <div>
+            <div className=" flex flex-col items-center justify-center font-semibold text-5xl gap-8 p-5 border-b-[1px]">
+                <h1>Tài khoản của bạn</h1>
+                <span className="bg-black p-[1.5px] w-14 flex items-center justify-center"></span>
+            </div>
+            <div className=" flex">
+                <div className=" w-1/4 p-5">
+                    <p className=" uppercase font-bold mb-3">Tài khoản</p>
+                    <div className=" mt-2"><a href="/account" className=" hover:text-blue-500 cursor-pointer">Thông tin tài khoản</a></div>
+                    <div className=" mt-2"><a href="/address" className=" hover:text-blue-500 cursor-pointer">Danh sách địa chỉ</a></div>
+                    <div onClick={() => handleLogOut()} className=" mt-2"><a className=" hover:text-blue-500 cursor-pointer">Đăng xuất</a></div>
                 </div>
-            </aside>
+                <div className=" w-3/4 p-5 mb-7">
+                    <div className=" uppercase font-bold mb-3 border-b-[1px] pb-1">Thông tin tài khoản</div>
 
-            <div class="w-full">
-                {content === 1 && <FormInfor/>}
-                {content === 2 && <Address/>}
-                {content === 3 && <OrderHistory/>}
+                    <div>
+                        {infor?.name && <p className=" font-medium">{infor?.name}</p>}
+                        {infor?.address && <div className=" text-sm">{infor?.address}</div>}
+                        {infor?.email && <div className=" text-sm">{infor?.email}</div>}
+                        <div className=" mt-1"><a className="hover:text-blue-500 cursor-pointer text-gray-500">Xem địa chỉ</a></div>
+                    </div>
+
+                    <div className=" mt-6">
+                        <div className=" uppercase font-semibold mb-3">Danh sách đơn hàng mới nhất</div>
+
+                    <div class="relative overflow-x-auto">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr className=" text-center">
+                                    <th scope="col" class="px-6 py-3">
+                                        Mã đơn hàng
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Ngày đặt
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Thành tiền
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Trạng thái thanh toán
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Vận chuyển
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className=" text-center">
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                                        <a className="hover:text-blue-500" href="#">#100325</a>
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        06/07/2023	
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        116,000₫
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Đã hoàn tất
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Đã giao hàng
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
