941f1ee2b277010f304956afcd54a51ef6a54e5a
vuhung1402 <badao867@gmail.com>
2024-04-20 22:40:00 +0700

    signup page

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,3 +1,4 @@
+import { json } from "react-router-dom";
 import { endpoint } from "../../api";
 import VerifyPopUp from "../../component/VerifyPopUp";
 import { useRef, useState } from "react";
@@ -7,6 +8,16 @@ function SignUp(){
     const passwordRef = useRef(null)
     const confirmPassRef = useRef(null)
 
+    const [inforUser, setInforUser] = useState({
+        ho: "",
+        ten: "",
+        gender: "",
+        birthday: "",
+        email: "",
+        password:"",
+    })
+
+    const [notifi, setNotifi] = useState()
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
     const [confirmPass, setConfirmPass] = useState()
@@ -21,194 +32,71 @@ function SignUp(){
     const [verifyPopUp, setVerifyPopUp] = useState(false)
 
     const handleSignIn =  () => {
-        // setVerifyPopUp(!verifyPopUp);
-        // console.log("email: ",email);
-        // console.log("pass: ",password);
-        // console.log("confirm: ",confirmPass);
-        const data = {
-            email: emailRef.current.value,
-            password: passwordRef.current.value,
-            confirmPassword: confirmPassRef.current.value,
-        }
-
-        if(password === confirmPass){
-            fetch(`${endpoint}/auth/register`, {
-            method:"POST",
-            body: JSON.stringify(data),
+        fetch(`${endpoint}/users/`,{
+            method: "POST",
+            body: JSON.stringify(inforUser),
             headers: {
                 'Content-Type': 'application/json',
-              },
-            }).then((response) => {
-                if(!response.ok){
-                    setErroreCode(response.status)
-                    throw new Error(`Netword response not ok ${response.status}`)
-                }
-                return response.json()
-            }).then((json) => {
-                if(json?.success){
-                    setCheckMailSent(true)
-                    console.log("respone: ", json)
-                }
-            }).catch((e) => {
-                setCheckMailSentErr(true)
-                console.log("e", e)
-            })
-        }else{
-            setCheckPass(true)
-        }
+            },
+        }).then((response) => {
+            if(!response.ok){
+                throw new Error(`Netword response not ok ${response.status}`)
+            }
+            return response.json()
+        }).then((json) => {
+            setNotifi(json)
+        }).catch((e) => {
+           console.log("e: ", e);
+        })
         
+        console.log("inforUser: ", inforUser)
     }
 
     return(
-        <div className="bg-gray-50 dark:bg-gray-900">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a
-                href="/"
-                className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-                >
-                <img
-                    className="w-8 h-8 mr-2"
-                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-                    alt="logo"
-                />
-                Flowbite
-                </a>
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Create and account
-                    </h1>
-                    <div className="space-y-4 md:space-y-6">
-                        <div>
-                            <label
-                                htmlFor="email"
-                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                            >
-                            Your email
-                            </label>
-                            <input
-                                type="email"
-                                name="email"
-                                id="email"
-                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="name@company.com"
-                                required=""
-                                ref={emailRef}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label
-                            htmlFor="password"
-                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                            >
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                name="password"
-                                id="password"
-                                placeholder="••••••••"
-                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required=""
-                                ref={passwordRef}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label
-                            htmlFor="confirm-password"
-                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                            >
-                            Confirm password
-                            </label>
-                            <input
-                                type="password"
-                                name="confirm-password"
-                                id="confirm-password"
-                                placeholder="••••••••"
-                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required=""
-                                ref={confirmPassRef}
-                                onChange={(e) => setConfirmPass(e.target.value)}
-                            />
-                        </div>
-                        <div className="flex items-start">
-                            <div className="flex items-center h-5">
-                            <input
-                                id="terms"
-                                aria-describedby="terms"
-                                type="checkbox"
-                                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                                required=""
-                            />
-                            </div>
-                            <div className="ml-3 text-sm">
-                            <label
-                                htmlFor="terms"
-                                className="font-light text-gray-500 dark:text-gray-300"
-                            >
-                                I accept the{" "}
-                                <a
-                                className="font-medium text-blue-700 hover:underline dark:text-primary-500"
-                                href="#"
-                                >
-                                Terms and Conditions
-                                </a>
-                            </label>
-                            </div>
-                        </div>
-                        {
-                            checkPass &&
-                            (
-                                <div className="mt-1 text-pink-600 text-sm text-xls font-medium">
-                                    Your password and confirm password do not match !!
-                                </div>
-                            )
-                        }
-                        {
-                            checkMailSent &&
-                            (
-                                <div className="mt-1 text-green-600 text-xls font-medium">
-                                    We've just sent you a verify email
-                                </div>
-                            )
-                        }
-                        {
-                            errorCode === 409 &&
-                            (
-                                <div className="mt-1 text-pink-600 text-sm text-xls font-medium">
-                                    Your email already exist !!!
-                                </div>
-                            )
-                        }
-                        <button
-                            onClick={handleSignIn}
-                            className="w-full text-white bg-sky-500	 hover:bg-sky-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                        >
-                            Create an account
-                        </button>
-                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Already have an account?{" "}
-                            <a
-                                href="/login"
-                                className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                            >
-                                Login here
-                            </a>
-                        </p>
+        <div className="flex">
+            <div className=" w-1/2 border-r-[1px] flex justify-center items-center font-semibold text-5xl">
+                Tạo tài khoản
+            </div>
+
+            <div className=" w-1/2 p-[100px]">
+                <input placeholder="Họ" className=" w-full outline-none border p-3 mb-4" value={inforUser.ho} onChange={e => setInforUser({...inforUser,ho: e.target.value})}/>
+
+                <input placeholder="Tên" className=" w-full outline-none border p-3 mb-4" value={inforUser.ten} onChange={e => setInforUser({...inforUser,ten: e.target.value})}/>
+
+                <div className=" flex gap-5 mb-4">
+                    <div className="flex items-center gap-2">
+                        <input className=" outline-none" type="radio" name="gender" value={"Nam"} onChange={e => setInforUser({...inforUser,gender: e.target.value})}/>
+                        <label>Nam</label>
+                        <input className=" outline-none" type="radio" name="gender" value={"Nữ"} onChange={e => setInforUser({...inforUser, gender: e.target.value})}/>
+                        <label>Nữ</label>
                     </div>
+
+                    
                 </div>
+
+                <input placeholder="dd/mm/yyyy" className=" w-full outline-none border p-3 mb-4" value={inforUser.birthday} onChange={e => setInforUser({...inforUser, birthday: e.target.value})}/>
+
+                <input placeholder="Email" className=" w-full outline-none border p-3 mb-4" value={inforUser.email} onChange={e => setInforUser({...inforUser, email: e.target.value})}/>
+
+                <input placeholder="Mật khẩu" type="password" className=" w-full outline-none border p-3 mb-4" value={inforUser.password} onChange={e => setInforUser({...inforUser, password: e.target.value})}/>
+
+                {
+                    notifi && 
+                    (
+                        <p className={`${notifi?.color}`}>{notifi?.message}</p>
+                    )
+                }
+
+                <div className=" mt-5 flex items-center"> 
+                    <button onClick={handleSignIn} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Đăng ký</button>
+                </div>
+                <div className=" flex gap-2 items-center mt-2 hover:text-blue-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+                    </svg>
+                    <a href="/" className=" cursor-pointer">Quay lại trang chủ </a>
                 </div>
             </div>
-            {/* {
-                respone?.success &&
-                (
-                    <div onClick={handleSignIn} className="fixed inset-0 bg-[rgba(184,184,184,0.35)] bg-opacity-90 flex justify-center items-center">
-                        <VerifyPopUp/>
-                    </div>
-                ) 
-            } */}
         </div>
 
     )
