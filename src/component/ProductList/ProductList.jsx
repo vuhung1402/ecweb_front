f6664f2ff6c7f6a3d7c66cbbf8b351077cce77df
jin <a11718trannhuy@gmail.com>
2024-06-15 17:29:27 +0700

    add tailwind breakpoint and update products page

--- src/component/ProductList/ProductList.jsx
+++ src/component/ProductList/ProductList.jsx
@@ -22,7 +22,7 @@ const ProductList = ({ handleSelect, data }) => {
 
                 {data?.length === 0 && <Empty />}
 
-                <div className=" grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-10">
+                <div className=" grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-4 xl:gap-x-10">
                     {
                         data?.map((item) => {
                             return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'ml': '900px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1400px',
+      '3xl': '1500px',
+    }
   },
   plugins: [
     require('tailwind-scrollbar-hide'),
