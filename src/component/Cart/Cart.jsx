751654d0c944ad1c7a6178e37b7dd8cd16f017a3
vuhung1402 <badao867@gmail.com>
2024-06-23 11:23:09 +0700

    update cart page

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -8,9 +8,13 @@ import CartCard from "./CartCard"
 import { data } from "@pages/admin/user/mock"
 import { getCart } from "./function"
 import { NOT_AUTHENTICATION, TOKEN_INVALID } from "@utils/error"
+import { useDispatch } from "react-redux"
+import { quantityCart } from "@pages/product/function"
+import { numOfCartPackage } from "@redux/actions"
 
 const Cart = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const [state, setState] = useState({
         data: undefined,
         quantity: '',
@@ -26,6 +30,7 @@ const Cart = () => {
         // setState((prev) => ({ ...prev }));
         if (cartItem?.success) {
             state.data = cartItem;
+            dispatch(numOfCartPackage(cartItem?.items?.length))
         } else {
             if (cartItem?.message === TOKEN_INVALID) {
                 logAgain();
@@ -39,15 +44,15 @@ const Cart = () => {
         <div className=" px-8 mb-1">
             <div className=" flex flex-col items-center justify-center font-semibold text-5xl gap-2 p-5 border-b-[1px]">
                 <div className=" font-semibold text-3xl">Giỏ hàng của bạn</div>
-                <div className=" font-normal text-sm">Có 1 sản phẩm trong giỏ hàng của bạn</div>
                 <span className="bg-black p-[1.5px] w-14"></span>
             </div>
             <div className=" flex p-4">
                 <div className=" w-1/2 h-[500px] overflow-y-auto">
+                    {state.data?.items?.length === 0 && <div className="">Giỏ hàng của bạn đang trống</div>}
                     {
                         state.data?.items?.map((item) => {
                             return (
-                                <CartCard data={item} />
+                                <CartCard data={item} getData={getData} />
                             )
                         })
                     }
@@ -57,7 +62,7 @@ const Cart = () => {
                         <h1 className=" border-b-[1px] text-lg font-semibold py-3">Thông tin đơn hàng</h1>
                         <div className=" flex items-center justify-between border-b-[1px] py-3">
                             <p className=" font-bold">Tổng tiền:</p>
-                            <p className=" text-red-600 font-semibold">{formatCurrencyVN(120000)}</p>
+                            <p className=" text-red-600 font-semibold">{formatCurrencyVN(state.data?.total_price)}</p>
                         </div>
                         <div className=" text-sm py-3">
                             Phí vận chuyển sẽ được tính ở trang thanh toán.
