c1447bbbe56107b0467822ffffcf72b5eccb12ab
vuhung1402 <badao867@gmail.com>
2024-06-23 11:24:07 +0700

    update function

--- src/pages/product/function.js
+++ src/pages/product/function.js
@@ -20,3 +20,48 @@ export const getDetailProduct = async (id) => {
         console.error('Error:', error);
     }
 }
+
+export const addToCart = async (body) => {
+    const token = localStorage.getItem("token");
+    try {
+        const response = await fetch(`${endpoint}/cart/add_to_cart`, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+                'token': token,
+            }
+        });
+        if (!response.ok) {
+            message.error("Rất tiếc, trang web đang bảo trì. Vui lòng quay lại sau");
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        message.error("Rất tiếc, trang web đang bảo trì. Vui lòng quay lại sau");
+        console.error('Error:', error);
+    }
+}
+
+export const quantityCart = async () => {
+    const token = localStorage.getItem("token");
+    try {
+        const response = await fetch(`${endpoint}/cart/show_number_items_in_cart`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'token': token,
+            }
+        });
+        if (!response.ok) {
+            message.error("Rất tiếc, trang web đang bảo trì. Vui lòng quay lại sau");
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        message.error("Rất tiếc, trang web đang bảo trì. Vui lòng quay lại sau");
+        console.error('Error:', error);
+    }
+}
