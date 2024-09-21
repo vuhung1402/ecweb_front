8bc733844e976208ad5fe0c067438d282ad3b074
jin <a11718trannhuy@gmail.com>
2024-09-21 09:26:41 +0700

    update code

--- src/component/ProductFilter/ProductFilter.jsx
+++ src/component/ProductFilter/ProductFilter.jsx
@@ -1,51 +1,28 @@
 import React from 'react';
 import { Select } from 'antd';
+
+import { userProductsFilter } from '@constants/index';
+
 import './ComponentStyle.css'
 
 const ProductFilter = ({handleSelect}) => {
 
-  return(
-      <Select
-        onSelect={handleSelect}
-        showSearch
-        style={{
-          width: 200,
-        }}
-        placeholder="Search to Select"
-        optionFilterProp="children"
-        filterOption={(input, option) => (option?.label ?? '').includes(input)}
-        filterSort={(optionA, optionB) =>
-          (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
-        }
-        options={[
-          {
-            value: 1,
-            label: 'Giá: Tăng dần',
-            name: "tang-dan"
-          },
-          {
-            value: 2,
-            label: 'Giá: Giảm dần',
-            name:'giam-dan'
-          },
-          {
-            value: 3,
-            label: 'Tên: A - Z',
-            name:'A-Z'
-          },
-          {
-            value:4,
-            label:'Tên: Z - A',
-            name:'Z-A'
-          },
-          {
-            value: 5,
-            label:'Mới nhất',
-            name:'moi-nhat'
-          }
-        ]}
-      />
-  )
+    return(
+        <Select
+            onSelect={handleSelect}
+            showSearch
+            style={{
+                width: 200,
+            }}
+            placeholder="Search to Select"
+            optionFilterProp="children"
+            filterOption={(input, option) => (option?.label ?? '').includes(input)}
+            filterSort={(optionA, optionB) =>
+                (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+            }
+            options={userProductsFilter}
+        />
+    )
 
 };
 export default ProductFilter;
--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,30 @@ export const GET_PRODUCTS_PAGE = 'get_products_page'
 export const navigatePath = {
     PRODUCT_ALL: '/products/all'
 };
+export const userProductsFilter = [
+    {
+        value: 1,
+        label: 'Giá: Tăng dần',
+        name: "tang-dan"
+    },
+    {
+        value: 2,
+        label: 'Giá: Giảm dần',
+        name:'giam-dan'
+    },
+    {
+        value: 3,
+        label: 'Tên: A - Z',
+        name:'A-Z'
+    },
+    {
+        value:4,
+        label:'Tên: Z - A',
+        name:'Z-A'
+    },
+    {
+        value: 5,
+        label:'Mới nhất',
+        name:'moi-nhat'
+    }
+];
